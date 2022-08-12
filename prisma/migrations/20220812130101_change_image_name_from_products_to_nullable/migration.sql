f538db29bb6bef56041d3e9f00855a1fdf2b9f64
Lucas Araújo <araujolucas3005@gmail.com>
2022-08-12 20:20:50 -0300

    deleted migrations

--- prisma/migrations/20220812130101_change_image_name_from_products_to_nullable/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `value` to the `products` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "products" ADD COLUMN     "value" INTEGER NOT NULL,
-ALTER COLUMN "imageName" DROP NOT NULL;
